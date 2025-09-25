59d486d31c10720b7aa8f8bce17c555ef96f5b55
Fideta <entreprisegassies@gmail.com>
2025-09-25 21:05:42 +0200

    Update principes-actifs.jsx

--- src/pages/principes-actifs.jsx
+++ src/pages/principes-actifs.jsx
@@ -7,7 +7,7 @@ import styles from "./principes-actifs.module.css";
 
 const ALL_CATEGORIES = [
   "Tous", "Vitamines", "Minéraux", "Acides gras", "Plantes",
-  "Protéines", "Acides aminés", "Neuro-hormone"
+  "Protéines", "Acides aminés", "Neuro-hormone", "Champignons"
 ];
 
 export default function PrincipesActifsPage() {
