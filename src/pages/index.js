c861a6aea007940dc799c3fd69bbab67544b6706
Fideta <entreprisegassies@gmail.com>
2025-12-17 22:29:28 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -83,13 +83,13 @@ export default function Home() {
                 <h3>Derniers produits analysés</h3>
                   <ul className={styles.productList}>
   <li>
+    <Link to="/docs/produits/ergytonic_multicebe">ERGYTONIC® Multicébé</Link> Les vitamines ne donnent pas de l’énergie. Elles corrigent des carences.
+  </li>
+  <li>
     <Link to="/docs/produits/ERGYPHILUS_Intima">ERGYPHILUS® Intima</Link> La flore vaginale par la bouche : une idée séduisante, des preuves limitées.
   </li>
   <li>
     <Link to="/docs/produits/BonjourCamomille">Bonjour Camomille</Link> 😴 Après le café, remplacer la tisane ?
-  </li>
-  <li>
-    <Link to="/docs/produits/florvis-sii">Florvis SII</Link> 😬 Pertinent ou pas pour le SII ?
   </li>
 </ul>
               </div>
