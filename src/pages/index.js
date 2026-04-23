90997f609ca6950580edebee5a2b6bd6d08d3bb5
Fideta <entreprisegassies@gmail.com>
2026-04-23 17:25:41 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -134,14 +134,13 @@ export default function Home() {
 
                     <ul className={styles.latestList}>
                       <li>
+                        <Link to="/docs/produits/hydrafizz-hydra">Hydrafizz Hydra</Link> Presque assez concentré.
+                      </li>
+                      <li>
                         <Link to="/docs/produits/hydrafizz-boost">Hydrafizz boost</Link> Petit coup de fouet possible.
                       </li>
                       <li>
                         <Link to="/docs/produits/azeol-gorge-spray">Azéol Gorge-Spray</Link> Peu de preuves robustes.
-                      </li>
-                      <li>
-                        <Link to="/docs/produits/cytocore">Cytocore</Link> Pas intense sur les
-                        preuves.
                       </li>
                     </ul>
 
