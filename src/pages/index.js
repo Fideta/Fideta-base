1a7d1e46062a6b8c871ca83563fe1405f2bcdce3
Fideta <entreprisegassies@gmail.com>
2026-06-30 23:27:01 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -134,13 +134,13 @@ export default function Home() {
 
                     <ul className={styles.latestList}>
                       <li>
+                        <Link to="/docs/produits/calm-my-mind">Kosbiotic Calm my mind</Link> Sans preuve d'efficacité.    
+                      </li>
+                      <li>
                         <Link to="/docs/produits/slim-my-body">Kosbiotic Slim my body</Link> Slim My Portefeuille, le body reste à démontrer.   
                       </li>
                       <li>
-                        <Link to="/docs/produits/glp1_protect">GLP1•PROTECT</Link> Il y a GLP-1 dans le titre, rien à craindre non ? 
-                      </li>
-                      <li>
-                        <Link to="/docs/produits/ergy-d">Ergy D</Link> N'est pas un médicament
+                        <Link to="/docs/produits/glp1_protect">GLP1•PROTECT</Link> Il y a GLP-1 dans le titre, rien à craindre non ?
                       </li>
                     </ul>
 
