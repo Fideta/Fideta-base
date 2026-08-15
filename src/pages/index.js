35c2cada9e1cfc2ccad570141e860c7f29900b20
Fideta <entreprisegassies@gmail.com>
2026-08-15 16:32:11 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -218,7 +218,7 @@ export default function Home() {
 
                     <ul className={styles.latestList}>
                       <li>
-                        <Link to="/docs/produits/luxeol-gummies-pousse-cheveux">Luxeol Gummies Pousse cheveux</Link> Au moins ça a bon goût.      
+                        <Link to="/docs/produits/luxeol_gummies_pousse_cheveux">Luxeol Gummies Pousse cheveux</Link> Au moins ça a bon goût.      
                       </li>
                       <li>
                         <Link to="/docs/produits/ergymag">Ergymag</Link> Un peu survendu.
